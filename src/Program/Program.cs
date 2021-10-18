4e9915e9980e0faa2a5616a27b63b8cd9f15037c
Néstor Martínez <nestor.marvi@gmail.com>
2021-10-18 20:40:39 -0300

    Configurada la Primera parte del ejercicio

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -8,6 +8,18 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PipeNull pipeNull = new PipeNull();
+            FilterNegative filterNegative = new FilterNegative();
+            PipeSerial pipeSerial2 = new PipeSerial(filterNegative,pipeNull);
+            FilterGreyscale filterGreyscale = new FilterGreyscale();
+            PipeSerial pipeSerial= new PipeSerial(filterGreyscale,pipeSerial2);
+
+            PictureProvider proveedor = new PictureProvider();
+            IPicture imagen = proveedor.GetPicture(@"luke.jpg");
+
+            pipeSerial.Send(imagen);
+            
+            proveedor.SavePicture(imagen,@"luke_distorted.jpg");
         }
     }
 }
