94007ba642688b70679cd74814683c9b9c0d4857
Néstor Martínez <nestor.marvi@gmail.com>
2021-10-20 14:39:40 -0300

    Ahora guarda imágenes en el medio del proceso utilizando pipeFork

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -11,13 +11,16 @@ namespace CompAndDel
             PipeNull pipeNull = new PipeNull();
             FilterNegative filterNegative = new FilterNegative();
             PipeSerial pipeSerial2 = new PipeSerial(filterNegative,pipeNull);
+            PipeFork pipeFork = new PipeFork(pipeSerial2,pipeNull);
             FilterGreyscale filterGreyscale = new FilterGreyscale();
-            PipeSerial pipeSerial= new PipeSerial(filterGreyscale,pipeSerial2);
+            PipeSerial pipeSerial= new PipeSerial(filterGreyscale,pipeFork);
 
             PictureProvider proveedor = new PictureProvider();
             IPicture imagen = proveedor.GetPicture(@"luke.jpg");
             
+
             proveedor.SavePicture(pipeSerial.Send(imagen),@"luke_distorted.jpg");
+            proveedor.SavePicture(pipeFork.Send(imagen),@"luke_negative.jpg");
         }
     }
 }
